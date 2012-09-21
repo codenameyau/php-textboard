d98ab384c3bc5fd4bf83b1e8f7838a96eac9c8ca
Jorge Yau <codenameyau@users.noreply.github.com>
2012-09-21 09:11:00 -0400

    Create user textboards, select boards, log files for boards

--- textboard/index.php
+++ textboard/index.php
@@ -1,21 +1,88 @@
 <?php
 require_once "headers/h1.php";
 
-$textboardfile = 'files/textboards/textboard1.txt';
-$editlogfile = 'files/logs/editlog'.$monthyear.'.txt';
+// Public Board
+if ($current_board == 0)
+{
+	$textboardfile = 'files/textboards/public.txt';
+	$editlogfile = 'files/textboard_logs/editlog'.$monthyear.'.txt';
 
-// Create textboard and log files
-if (!file_exists($textboardfile))
-	{
-		$txt1 = fopen($textboardfile, 'a');
-		fclose($txt1);
-	}
+	if ($account_type != 'admin' && $account_type != 'user')
+		$readonly = 'readonly';
 
-if (!file_exists($editlogfile))
-	{
-		$edittxt = fopen($editlogfile, 'a');
-		fclose($edittxt);
-	}
+	if (!is_dir('files/textboard_logs'))
+		mkdir('files/textboard_logs');
+
+	// Create textboard and log files
+	if (!file_exists($textboardfile))
+		{
+			$txt1 = fopen($textboardfile, 'a');
+			fclose($txt1);
+		}
+
+	if (!file_exists($editlogfile))
+		{
+			$edittxt = fopen($editlogfile, 'a');
+			fclose($edittxt);
+		}
+}
+
+// Announcements Board
+else if ($current_board == 1)
+{
+	$textboardfile = 'files/textboards/announcements.txt';
+	$editlogfile = 'files/announce_logs/editlog'.$monthyear.'.txt';
+
+	if ($account_type != 'admin')
+		$readonly = 'readonly';
+
+	// Create textboard and log files
+	if (!is_dir('files/announce_logs'))
+		mkdir('files/announce_logs');
+
+	if (!file_exists($textboardfile))
+		{
+			$txt1 = fopen($textboardfile, 'a');
+			fclose($txt1);
+		}
+
+	if (!file_exists($editlogfile))
+		{
+			$edittxt = fopen($editlogfile, 'a');
+			fclose($edittxt);
+		}
+}
+
+// Userboard
+else
+{
+	// if GET url_supplied
+
+
+	// if no Get url
+	$user_dir   = substr($board_id, 6, 20);
+	$user_board = substr($board_id, 0, 5);
+
+	$textboardfile = "boards/$user_dir/$user_board.txt";
+	$editlogfile = "boards/$user_dir/logs/editlog'.$monthyear.'.txt";
+
+	if ($account_type != 'admin' || $account_type != 'user')
+		$readonly = 'readonly';
+
+	// Create textboard and log files
+	if (!file_exists($textboardfile))
+		{
+			$txt1 = fopen($textboardfile, 'a');
+			fclose($txt1);
+		}
+
+	if (!file_exists($editlogfile))
+		{
+			$edittxt = fopen($editlogfile, 'a');
+			fclose($edittxt);
+		}
+}
+
 
 if (isset($_POST['button']) && $username != 'Guest')
 	{
@@ -37,41 +104,40 @@ $txtfile = fopen($textboardfile, 'r');
 $filecontents = fread($txtfile, filesize($textboardfile)+1);
 
 if ($account_type == 'admin')
-	echo 	"<span style='margin-left:40px'>
+	echo 	"<span class='menu'>
 				<a href='session.php'>$username</a>
+				<a href='logs.php' target='_blank'>logs</a>
+				<a href='database.php'>database</a>
+			<form action='' method='POST' style='display: inline;'>
+			<select name='select_board'>
+				<option value='public'>public</option>
+				<option value='announcements'>announcements</option>
+				<option value='user'>$username</option>
+			</select>
+			<input type='submit' class='button' name='button' value='update'>
 			</span>
-			<span style='margin-left:35px''>
-			<a href='logs.php' target='_blank'>logs</a></span>
-			<span style='margin-left:35px;margin-right:30px''>
-			<a href='database.php'>database</a></span>
-			<form action='' method='POST' style='display: inline;'>
-			<input type='submit' class='button' name='button' value='update'>
-			<textarea rows='30' name='edittext' spellcheck='false'>$filecontents</textarea>
-			</form>";
-
-else if ($account_type == 'mod')
-	echo 	"<span style='margin-left:40px'>
-				<a href='session.php'>$username</a></span>
-			<span style='margin-left:35px;margin-right:30px''>
-			<a href='logs.php' target='_blank'>logs</a></span>
-			<form action='' method='POST' style='display: inline;'>
-			<input type='submit' class='button' name='button' value='update'>
-			<textarea rows='30' name='edittext' spellcheck='false'>$filecontents</textarea>
+			<textarea rows='30' name='edittext' spellcheck='false' $readonly>$filecontents</textarea>
 			</form>";
 
 else if ($account_type == 'user')
-	echo 	"<span style='margin-left:40px;margin-right:30px'>
-			<a href='session.php'>$username</a></span>
-			<form action='' method='POST' style='display: inline;'>
-			<input type='submit' class='button' name='button' value='update'>
-			<textarea rows='30' name='edittext' spellcheck='false'>$filecontents</textarea>
+	echo 	"<span class='menu'>
+				<a href='session.php'>$username</a>
+				<form action='' method='POST' style='display: inline;'>
+				<select name='select_board'>
+					<option value='public'>public</option>
+					<option value='announcements'>announcements</option>
+					<option value='user'>$username</option>
+				</select>
+				<input type='submit' class='button' name='button' value='update'>
+			</span>
+			<textarea rows='30' name='edittext' spellcheck='false' $readonly>$filecontents</textarea>
 			</form>";
 
 else
-	echo 	"<span style='margin-left:40px'>
-			<a href='session.php'>$username</a>
-			</span><form action='' method='POST'>
-			<textarea rows='30' name='edittext' readonly>$filecontents</textarea></form>";
+	echo 	"<span class='menu'>
+				<a href='session.php'>$username</a>
+			</span>
+			<textarea rows='30' spellcheck='false' $readonly>$filecontents</textarea>";
 
 ?>
 <br>
