cf5772eea095fd602cfe4029a0bcde6cfa909035
Jorge Yau <codenameyau@users.noreply.github.com>
2012-09-23 23:32:14 -0400

    Added log file scripts

---
+++ textboard/scripts/log_00.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+header("Content-Type: text/plain");
+$account_type = $_SESSION['account'];
+
+if ($account_type == 'admin' && isset($_SESSION['view_log']))
+{
+	date_default_timezone_set('America/New_York');
+	$time = time();
+	$year = date('Y');
+
+	$public_log_dir = "../files/logs_00/$year/";
+	$log_date = $_SESSION['view_log'];
+
+	$monthalpha = date('F Y', $time);
+	
+	$txtfile = "$public_log_dir$log_date.txt";
+	if (filesize($txtfile) > 0)
+		$txtfiledata = gzuncompress(file_get_contents($txtfile));
+	else exit("No logs recorded yet.");
+	
+	echo "Public Board: $monthalpha\n";
+	echo $txtfiledata;
+}
+
+else header("Location: index.php");
+?>
+
